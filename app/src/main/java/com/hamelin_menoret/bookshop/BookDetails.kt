5563634d96e379a3de4e1702d2fd9b4097b036e5
azertyValentin <etu.valentin.menoret@gmail.com>
2021-10-24 23:11:28 +0200

    :sparkles: add cart managment

--- app/src/main/java/com/hamelin_menoret/bookshop/BookDetails.kt
+++ app/src/main/java/com/hamelin_menoret/bookshop/BookDetails.kt
@@ -1,6 +1,8 @@
 package com.hamelin_menoret.bookshop
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +10,18 @@ import com.squareup.picasso.Picasso
 
 class BookDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val global: Global = this.applicationContext as Global
+        val cart = global.globalCart
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.book_details)
+
         val image = findViewById<ImageView>(R.id.image)
         val title = findViewById<TextView>(R.id.title)
         val isbn = findViewById<TextView>(R.id.isbn)
         val price = findViewById<TextView>(R.id.price)
         val synopsis = findViewById<TextView>(R.id.synopsis)
+        val addToCartbutton = findViewById<Button>(R.id.addToCart)
 
         val bookTitle = intent.getStringExtra("BOOK_TITLE");
         val bookCover = intent.getStringExtra("BOOK_COVER");
@@ -27,5 +34,21 @@ class BookDetails : AppCompatActivity() {
         price.text = bookPrice
         synopsis.text = bookSynopsis
         Picasso.get().load(bookCover).into(image)
+
+        if (cart.find { it.equals(bookTitle)} != null) {
+            addToCartbutton?.isEnabled = false
+            addToCartbutton?.setTextColor(Color.GRAY)
+            addToCartbutton?.text = "Le livre est dans votre panier"
+        }
+
+        addToCartbutton?.setOnClickListener {
+            if(bookTitle != null) {
+                cart.add(bookTitle)
+                global.globalCart = cart
+                addToCartbutton?.isEnabled = false
+                addToCartbutton?.setTextColor(Color.GRAY)
+                addToCartbutton?.text = "Le livre est dans votre panier"
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hamelin_menoret/bookshop/Global.kt
@@ -0,0 +1,7 @@
+package com.hamelin_menoret.bookshop
+
+import android.app.Application
+
+class Global: Application()  {
+    var globalCart: ArrayList<String> = ArrayList()
+}
